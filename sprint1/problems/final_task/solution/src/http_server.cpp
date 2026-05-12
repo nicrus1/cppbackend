217ee1a3faaec2adbeb8eeabea44cf77ab8b48e2
nicrus <maewyd0@gmail.com>
2026-05-12 15:30:46 +0300

    addd3

--- sprint1/problems/final_task/solution/src/http_server.cpp
+++
@@ -1,6 +0,0 @@
-#include "http_server.h"
-
-namespace http_server {
-// Все реализации http-сервера находятся в заголовочном файле http_server.h
-// Данный файл существует для компиляции модуля
-}  // namespace http_server
--- sprint1/problems/final_task/solution/src/request_handler.cpp
+++
@@ -1,5 +0,0 @@
-#include "request_handler.h"
-
-namespace http_handler {
-// Все реализации находятся в заголовочном файле
-}  // namespace http_handler
