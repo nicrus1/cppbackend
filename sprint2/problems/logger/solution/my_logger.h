0f972867420482529bbe7c2377eb6428495ab4f0
nicrus <maewyd0@gmail.com>
2026-05-17 19:42:04 +0300

    logger4

--- sprint2/problems/logger/solution/my_logger.h
+++ sprint2/problems/logger/solution/my_logger.h
@@ -26,43 +26,53 @@ class Logger {
     std::string GetTimeStamp() const {
         const auto now = GetTime();
         const auto t_c = std::chrono::system_clock::to_time_t(now);
+
         std::tm tm;
 #ifdef _WIN32
         localtime_s(&tm, &t_c);
 #else
         localtime_r(&t_c, &tm);
 #endif
+
         std::ostringstream oss;
         oss << std::put_time(&tm, "%F %T");
         return oss.str();
     }
-    
+
     std::string GetLogFileName() const {
         const auto now = GetTime();
         const auto t_c = std::chrono::system_clock::to_time_t(now);
+
         std::tm tm;
 #ifdef _WIN32
         localtime_s(&tm, &t_c);
 #else
         localtime_r(&t_c, &tm);
 #endif
+
         std::ostringstream oss;
-        oss << "log_" << std::put_time(&tm, "%Y_%m_%d") << ".txt";
+        oss << "/var/log/sample_log_"
+            << std::put_time(&tm, "%Y_%m_%d")
+            << ".log";
+
         return oss.str();
     }
-    
+
     void RotateFileIfNeeded() {
         std::string new_filename = GetLogFileName();
+
         if (!log_file_.is_open() || current_filename_ != new_filename) {
             if (log_file_.is_open()) {
                 log_file_.close();
             }
+
             current_filename_ = new_filename;
             log_file_.open(current_filename_, std::ios::app);
         }
     }
 
     Logger() = default;
+
     Logger(const Logger&) = delete;
     Logger& operator=(const Logger&) = delete;
 
@@ -72,20 +82,25 @@ public:
         return instance;
     }
 
-    void LogImpl(std::ostream& os) const {}
+    void LogImpl(std::ostream& os) const {
+    }
 
-    template<typename T, typename... Ts>
+    template <typename T, typename... Ts>
     void LogImpl(std::ostream& os, const T& arg, const Ts&... args) const {
         os << arg;
         LogImpl(os, args...);
     }
 
-    template<class... Ts>
+    template <class... Ts>
     void Log(const Ts&... args) {
         std::lock_guard<std::mutex> lock(m_);
+
         RotateFileIfNeeded();
+
         log_file_ << GetTimeStamp() << ": ";
+
         LogImpl(log_file_, args...);
+
         log_file_ << std::endl;
     }
 
