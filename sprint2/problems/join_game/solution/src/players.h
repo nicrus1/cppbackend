a6f2c701c85fc2cd62b818b486b3767838227b27
nicrus <maewyd0@gmail.com>
2026-05-22 02:30:15 +0300

    gaammmeee1

--- sprint2/problems/join_game/solution/src/players.h
+++ sprint2/problems/join_game/solution/src/players.h
@@ -10,90 +10,134 @@ namespace model {
 class Players {
 public:
     Players() = default;
-    
+
     Players(const PlayerTokens& tokens) = delete;
     Players& operator=(const PlayerTokens& tokens) = delete;
-    
+
     Player& AddPlayer(std::string name, const Map::Id& map_id) {
         PlayerId new_id{next_id_++};
-        auto player = std::make_unique<Player>(new_id, std::move(name), map_id);
+
+        auto player = std::make_unique<Player>(
+            new_id,
+            std::move(name),
+            map_id
+        );
+
         Player& ref = *player;
-        players_[new_id] = std::move(player);
+
+        players_.emplace(new_id, std::move(player));
         map_players_[map_id].push_back(new_id);
+
         return ref;
     }
-    
+
     Token GenerateToken(const Player& player) {
         return player_tokens_.GenerateToken(player);
     }
-    
+
     Player* FindPlayer(PlayerId id) {
         auto it = players_.find(id);
-        return it != players_.end() ? it->second.get() : nullptr;
+
+        if (it == players_.end()) {
+            return nullptr;
+        }
+
+        return it->second.get();
     }
-    
+
     const Player* FindPlayer(PlayerId id) const {
         auto it = players_.find(id);
-        return it != players_.end() ? it->second.get() : nullptr;
+
+        if (it == players_.end()) {
+            return nullptr;
+        }
+
+        return it->second.get();
     }
-    
+
+    // ИСПРАВЛЕНО
+    // Нельзя считать PlayerId{0} невалидным,
+    // потому что первый игрок имеет id = 0
     Player* FindPlayerByToken(const Token& token) {
-        PlayerId player_id = player_tokens_.FindPlayerByToken(token);
-        if (player_id == PlayerId{0}) {
+        if (!player_tokens_.IsValidToken(token)) {
             return nullptr;
         }
+
+        PlayerId player_id = player_tokens_.FindPlayerByToken(token);
+
         return FindPlayer(player_id);
     }
-    
+
     const Player* FindPlayerByToken(const Token& token) const {
-        PlayerId player_id = player_tokens_.FindPlayerByToken(token);
-        if (player_id == PlayerId{0}) {
+        if (!player_tokens_.IsValidToken(token)) {
             return nullptr;
         }
+
+        PlayerId player_id = player_tokens_.FindPlayerByToken(token);
+
         return FindPlayer(player_id);
     }
-    
+
     bool ValidateToken(const Token& token) const {
         return player_tokens_.IsValidToken(token);
     }
-    
+
     std::vector<Player*> GetPlayersOnMap(const Map::Id& map_id) {
         std::vector<Player*> result;
+
         auto it = map_players_.find(map_id);
+
         if (it != map_players_.end()) {
             for (auto player_id : it->second) {
                 Player* player = FindPlayer(player_id);
+
                 if (player) {
                     result.push_back(player);
                 }
             }
         }
+
         return result;
     }
-    
+
     std::vector<const Player*> GetPlayersOnMap(const Map::Id& map_id) const {
         std::vector<const Player*> result;
+
         auto it = map_players_.find(map_id);
+
         if (it != map_players_.end()) {
             for (auto player_id : it->second) {
                 const Player* player = FindPlayer(player_id);
+
                 if (player) {
                     result.push_back(player);
                 }
             }
         }
+
         return result;
     }
-    
+
     bool HasPlayer(PlayerId id) const {
         return players_.find(id) != players_.end();
     }
 
 private:
-    uint64_t next_id_{0};
+    uint64_t next_id_ = 0;
+
     PlayerTokens player_tokens_;
-    std::unordered_map<PlayerId, std::unique_ptr<Player>, util::TaggedHasher<PlayerId>> players_;
-    std::unordered_map<Map::Id, std::vector<PlayerId>, util::TaggedHasher<Map::Id>> map_players_;
+
+    std::unordered_map<
+        PlayerId,
+        std::unique_ptr<Player>,
+        util::TaggedHasher<PlayerId>
+    > players_;
+
+    std::unordered_map<
+        Map::Id,
+        std::vector<PlayerId>,
+        util::TaggedHasher<Map::Id>
+    > map_players_;
 };
 
 }  // namespace model
