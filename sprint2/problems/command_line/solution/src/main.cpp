d56c85174a092007ff300bf99334b0dffd83b33e
nicrus <maewyd0@gmail.com>
2026-06-02 01:16:26 +0300

    fixfixifxfi

--- sprint2/problems/command_line/solution/src/main.cpp
+++ sprint2/problems/command_line/solution/src/main.cpp
@@ -1,6 +1,8 @@
 #include "sdk.h"
+
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/signal_set.hpp>
+
 #include <iostream>
 #include <thread>
 #include <chrono>
@@ -8,8 +10,10 @@
 #include "json_loader.h"
 #include "request_handler.h"
 #include "logger.h"
+#include "command_line.h"
 
 using namespace std::literals;
+
 namespace net = boost::asio;
 using tcp = net::ip::tcp;
 
@@ -18,11 +22,14 @@ namespace {
 template <typename Fn>
 void RunWorkers(unsigned n, const Fn& fn) {
     n = std::max(1u, n);
+
     std::vector<std::jthread> workers;
     workers.reserve(n - 1);
+
     while (--n) {
         workers.emplace_back(fn);
     }
+
     fn();
 }
 
@@ -30,64 +37,60 @@ void RunWorkers(unsigned n, const Fn& fn) {
 
 int main(int argc, const char* argv[]) {
     logger::InitLogging();
-    
-    std::cerr << "=== Game Server Starting ===" << std::endl;
-    
-    if (argc != 2) {
-        std::cerr << "Usage: game_server <game-config-json>" << std::endl;
-        return EXIT_FAILURE;
-    }
-    
+
     int exit_code = EXIT_SUCCESS;
     std::string exception_msg;
-    
+
     try {
-        std::cerr << "Loading game config from: " << argv[1] << std::endl;
-        model::Game game = json_loader::LoadGame(argv[1]);
-        std::cerr << "Game config loaded successfully" << std::endl;
+        auto args_opt = ParseCommandLine(argc, argv);
+
+        if (!args_opt) {
+            return EXIT_SUCCESS;
+        }
+
+        const Args args = *args_opt;
+
+        model::Game game = json_loader::LoadGame(args.config_file);
 
         const unsigned num_threads = std::thread::hardware_concurrency();
-        std::cerr << "Using " << num_threads << " threads" << std::endl;
-        
+
         net::io_context ioc(num_threads);
 
         net::signal_set signals(ioc, SIGINT, SIGTERM);
-        signals.async_wait([&ioc](const boost::system::error_code&, int signal) {
-            std::cerr << "Received signal " << signal << ", stopping server..." << std::endl;
+        signals.async_wait([&ioc](const boost::system::error_code&, int) {
             ioc.stop();
         });
 
-        std::cerr << "Creating RequestHandler..." << std::endl;
-        http_handler::RequestHandler handler{game};
-        std::cerr << "RequestHandler created" << std::endl;
+        http_handler::RequestHandler handler{
+            game,
+            args.www_root
+        };
 
         const auto address = net::ip::make_address("0.0.0.0");
-        const unsigned short port = 8080;
-        
-        logger::LogServerStarted(address.to_string(), port);
-        std::cerr << "Starting HTTP server on " << address.to_string() << ":" << port << std::endl;
-        
-        http_server::ServeHttp(ioc, {address, port}, [&handler](auto&& req, auto&& send) {
-            handler(std::forward<decltype(req)>(req), std::forward<decltype(send)>(send));
-        });
+        constexpr unsigned short port = 8080;
 
-        std::cerr << "Server is running. Press Ctrl+C to stop." << std::endl;
-        
+        logger::LogServerStarted(address.to_string(), port);
+
+        http_server::ServeHttp(
+            ioc,
+            {address, port},
+            [&handler](auto&& req, auto&& send) {
+                handler(
+                    std::forward<decltype(req)>(req),
+                    std::forward<decltype(send)>(send)
+                );
+            });
+
         RunWorkers(std::max(1u, num_threads), [&ioc] {
             ioc.run();
         });
-        
-        std::cerr << "Server stopped" << std::endl;
-        
+
     } catch (const std::exception& ex) {
         exit_code = EXIT_FAILURE;
         exception_msg = ex.what();
-        std::cerr << "FATAL ERROR: " << exception_msg << std::endl;
     }
-    
+
     logger::LogServerExited(exit_code, exception_msg);
-    
-    std::cerr << "Server exited with code " << exit_code << std::endl;
-    
+
     return exit_code;
 }
